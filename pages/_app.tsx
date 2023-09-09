4edf8db68f76e37f39432d073d18b09c8ccec03b
Rahuletto <rahulmarban@gmail.com>
2023-09-09 12:36:08 +0000

    right panel and search

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { DM_Sans } from "next/font/google";
+
+const dm = DM_Sans({
+  subsets: ["latin"],
+  weight: ["500", "700"],
+  display: "swap",
+  style: ["normal"],
+  variable: "--main-font",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          html {
+            --main-font: ${dm.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
